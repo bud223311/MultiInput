3528596108b074c045eb9cde9199df612e8071d3
MrKipling42 <119621368+MrKipling42@users.noreply.github.com>
2026-03-31 08:10:18 +0100

    BREAKING CHANGE Major Code Refactoring

--- MultiInput/Program.cs
+++ MultiInput/Program.cs
@@ -1,20 +1,17 @@
-﻿using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using System.Text.RegularExpressions;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿using System.Text.RegularExpressions;
+using MultiInput.TCP;
 
-namespace ConsoleApp1;
+namespace MultiInput;
 
-internal class Server
+internal class TcpServer
 {
     //[DllImport("user32.dll")]
     //static extern bool PostMessage(HandleRef hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
 
     internal const bool Debug = true;
-    private static uint hwnd = 0;
-    private static string RunTime = string.Format("{0:D}", System.DateTime.Now);
-    private static readonly string LogPath = Path.Combine(AppContext.BaseDirectory, $"log {RunTime.Replace(':', '.')}.txt");
+    private static uint _hwnd;
+    private static readonly string RunTime = $"{DateTime.Now:D}";
+    internal static readonly string LogPath = Path.Combine(AppContext.BaseDirectory, $"log {RunTime.Replace(':', '.')}.txt");
 
     //Launch args validation and handling
     public static void Main(string[] args)
@@ -26,115 +23,29 @@ internal class Server
             Console.WriteLine("No arguments found...");
             throw new Exception("No arguments provided. Please use the attached executable.");
         }
+        _hwnd = uint.Parse(args[0]);
+        string launchType = args[1];
+        string ip = args[2];
+        int port = int.Parse(args[3]);
 
-        hwnd = uint.Parse(args[0]);
-        string LaunchType = args[1];
-        string IP = args[2];
-        int Port = int.Parse(args[3]);
-
-        if (!Regex.IsMatch(hwnd.ToString(), "[0-9]{6,7}"))
+        if (!Regex.IsMatch(_hwnd.ToString(), "[0-9]{6,7}"))
         {
-            Console.WriteLine($"HWND ({hwnd}) specified, connecting to AHK...");
+            Console.WriteLine($"HWND ({_hwnd}) specified, connecting to AHK...");
         }
         //add hwnd validity check unless in debug
 
-
-
-        switch (LaunchType)
+        switch (launchType)
         {
             case "receiver":
-                ServerListener(Port);
+                //Start TCP client (Receiver)
+                TcpReceiver.ServerListener(port);
                 break;
             case "sender":
-                ServerSender(IP, Port);
+                //Start TCP server (Sender)
+                TcpSender.ServerSender(ip, port);
                 break;
         }
     }
-
-
-    //Start TCP client (Receiver)
-    public static void ServerListener(int Port)
-    {
-        TcpListener server = new(IPAddress.Any, Port);
-
-        FileStream fs = new(LogPath, FileMode.OpenOrCreate, FileAccess.Write, FileShare.Read, bufferSize: 8);
-        {
-            //Get newline byte value for formatting log
-            var newlinetxt = Environment.NewLine;
-            byte[] newline = Encoding.UTF8.GetBytes(newlinetxt);
-
-            //Position file pointer to new line at file end
-            fs.Position = fs.Seek(0, SeekOrigin.End);
-            fs.WriteAsync(newline, 0, newlinetxt.Length);
-
-            server.Start();
-            Console.WriteLine($"Waiting for connection on port: {Port}");
-
-            while (true)
-            {
-                TcpClient client = server.AcceptTcpClient();
-                NetworkStream stream = client.GetStream();
-                
-                byte[] buffer = new byte[8];
-                int bytesRead = stream.Read(buffer, 0, buffer.Length);
-                
-                string data = Encoding.UTF8.GetString(buffer, 0, bytesRead);
-                byte[] message = new byte[data.Length];
-                for (int i = 0; i < message.Length; i++)
-                {
-                    message[i] = buffer[i];
-                }
-                Console.WriteLine($"Received: {data}");
-
-                //write newline before each new log entry
-                fs.Write(newline, 0, newlinetxt.Length);
-                fs.Write(message, 0, message.Length);
-
-                Console.WriteLine($"{buffer[0]}");
-
-                client.Close();
-            }
-        }
-    }
-
-    //Start TCP server (Sender)
-    private static void ServerSender(string IP, int Port)
-    {
-        FileStream fs = new(LogPath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite, bufferSize: 8);
-        {
-            var newlinetxt = Environment.NewLine;
-            byte[] newline = Encoding.UTF8.GetBytes(newlinetxt);
-
-            fs.Position = fs.Seek(0, SeekOrigin.End);
-            fs.WriteAsync(newline, 0, newlinetxt.Length);
-            //debug
-
-            Console.WriteLine("Ready to send data.");
-            while (true)
-            {
-                //add read logpath stream for getting data from ahk
-
-                TcpClient client = new(IP, Port);
-                NetworkStream stream = client.GetStream();
-
-
-                int key = fs.ReadByte();
-
-
-
-                byte[] data = Encoding.UTF8.GetBytes(key.ToString());
-                stream.Write(data, 0, data.Length);
-
-                client.Close();
-            }
-        }
-    }
-
-
-
-
-
-
 }
 /*
  TODO:
---
+++ MultiInput/TCP/Sender.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace MultiInput.TCP;
+
+internal class TcpSender
+{
+    internal static void ServerSender(string ip, int port)
+    {
+        FileStream fs = new(TcpServer.LogPath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite, bufferSize: 8);
+        {
+            var newlineString = Environment.NewLine;
+            byte[] newline = Encoding.UTF8.GetBytes(newlineString);
+
+            fs.Position = fs.Seek(0, SeekOrigin.End);
+            fs.WriteAsync(newline, 0, newlineString.Length);
+            //debug
+
+            Console.WriteLine("Ready to send data.");
+            while (true)
+            {
+                //add read log path stream for getting data from ahk
+
+                TcpClient client = new(ip, port);
+                NetworkStream stream = client.GetStream();
+
+
+                int key = fs.ReadByte();
+
+
+
+                byte[] data = Encoding.UTF8.GetBytes(key.ToString());
+                stream.Write(data, 0, data.Length);
+
+                client.Close();
+            }
+        }
+    }
+}
