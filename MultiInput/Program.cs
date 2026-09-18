c1ac68b15fd0fc469b5080cc55c36d4b061cedf7
MrKipling42 <119621368+MrKipling42@users.noreply.github.com>
2026-09-18 15:46:40 +0100

    Add Hook for when process is killed Utilization of Debug Logs Add exit Command

--- MultiInput/Program.cs
+++ MultiInput/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using MultiInput.Extensions;
+using MultiInput.Logging;
 using MultiInput.TCP;
 
 namespace MultiInput;
@@ -15,24 +17,33 @@ internal static class TcpServer
 
     //Launch args validation and handling
     public static void Main(string[] args){
+        handler = new ConsoleEventDelegate(OnCloseProgram);
+        SetConsoleCtrlHandler(handler, true);
+        DebugLog.InitializeStartup();
+        ConsoleLog.WriteConsoleMessage($"MultiInput TCP Server v1.0.0\nStarted at: {RunTime}\nDebug Log Path: {DebugLog.GetPathOfDebugLog()}", ConsoleColor.Cyan);
         while (!EndProgram) {
-            Console.WriteLine($"Connect to a machine running this application using:\nip:port e.g(127.0.0.1:7777)\nOr start accepting Connections using\naccept or a");
+            ConsoleLog.WriteConsoleMessage($"Connect to a machine running this application using: ip:port e.g (127.0.0.1:7777)\nOr start accepting Connections using accept or a", ConsoleColor.Green);
             string consoleStr = Console.ReadLine() ?? string.Empty;
-            
+            DebugLog.DebugMessageThread($"Console Input: {consoleStr}");
+            if (consoleStr.ToLowerInvariant() is "exit" or "e" or "quit" or "q") {
+                EndProgram = true;
+                continue;
+            }
             
             if (consoleStr.ToLowerInvariant() is "accept" or "a" or "acc") {
-                Console.WriteLine($"Port in which to accept on?");
+                ConsoleLog.WriteConsoleMessage($"Port in which to accept on?",ConsoleColor.Yellow);
                 string port = Console.ReadLine() ?? string.Empty;
                 if (!int.TryParse(port,out int tPort)) {
                     continue;
                 }
-                
+                DebugLog.DebugMessageThread($"Accepting Connections on Port: {tPort}");
                 StaticData.Host = new MultiInputTcp.KTcpHost(TcpListener.Create(tPort));
                 StaticData.Host.Awake();
                 StaticData.WasDisconnected = false;
                 while (!StaticData.WasDisconnected) {
                     StaticData.Host.Update();
                 }
+                DebugLog.DebugMessageThread($"Stopped Accepting Connections on Port: {tPort}");
                 StaticData.Host.Close();
                 continue;
             }
@@ -56,14 +67,27 @@ internal static class TcpServer
                 Console.WriteLine($"Port TryParse Failed: {consoleStr}");
                 continue;
             }
-
+            DebugLog.DebugMessageThread($"Connecting to {address}:{portresult}");
             StaticData.Client = new MultiInputTcp.KTcpClient(new TcpClient(),address,portresult);
             StaticData.Client.Awake();
             StaticData.WasDisconnected = false;
             while (!StaticData.WasDisconnected) {
                 StaticData.Client.Update();
             }
+            DebugLog.DebugMessageThread($"Disconnected from {address}:{portresult}");
             StaticData.Client.Close();
         }
+        OnCloseProgram(1);
     }
+    private static bool OnCloseProgram(int eventType) {
+        ConsoleLog.WriteConsoleMessage($"Closing Program...", ConsoleColor.Red);
+        DebugLog.CreatePreviousDebugLog();
+        return false;
+    }
+    
+    static ConsoleEventDelegate handler;   // Keeps it from getting garbage collected
+    // Pinvoke
+    private delegate bool ConsoleEventDelegate(int eventType);
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern bool SetConsoleCtrlHandler(ConsoleEventDelegate callback, bool add);
 }
