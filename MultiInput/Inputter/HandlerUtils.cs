74970b064438c7a029d39d0191e5b2473d07b472
MrKipling42 <119621368+MrKipling42@users.noreply.github.com>
2026-09-24 01:12:43 +0100

    Utilisation of the Data Class with new Function to Validate it including ping checking
    Moved the Controller stuff to a new Function
    
    renamed PressedKeys to private friendly format

---
+++ MultiInput/Inputter/HandlerUtils.cs
@@ -0,0 +1,82 @@
+﻿using MultiInput.Logging;
+using WindowsInput.Native;
+
+namespace MultiInput.Inputter;
+
+public static class HandlerUtils
+{
+    public static Data? ValidateData(string data){
+        if (data.Equals("ping", StringComparison.InvariantCultureIgnoreCase)) {
+            Console.WriteLine($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| Alive Ping Received");
+            DebugLog.WriteDebugMessage($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| Alive Ping Received");
+            return null;
+        }
+        
+        
+        var strs = data.Split('.');
+        string fKey = strs[1];
+        
+        
+        if (!int.TryParse(strs[0], out int inputType)) {
+            Console.WriteLine($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| Failed To Parse Input Type. DATA:{data}");
+            DebugLog.WriteDebugMessage($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| Failed To Parse Input Type. DATA:{data}");
+            return null;
+        }
+            
+        if (fKey == string.Empty) {
+            Console.WriteLine($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| Failed To Parse Key. DATA:{data}");
+            DebugLog.WriteDebugMessage($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| Failed To Parse Key. DATA:{data}");
+            return null;
+        }
+        if (!int.TryParse(strs[2], out int fState)) {
+            Console.WriteLine($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| Failed To Parse State. DATA:{data}");
+            DebugLog.WriteDebugMessage($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| Failed To Parse State. DATA:{data}");
+            return null;
+        }
+
+        return new Data((InputType)inputType,fKey,fState);
+    }
+
+    public static void HandleController(Data data){
+        switch (data.Key) {
+            /*case $"A":
+                KeyboardInput.SendKeyStroke((int)VirtualKeyCode.SPACE, fState);
+                break;
+            case $"B":
+                KeyboardInput.SendKeyStroke((int)VirtualKeyCode.LCONTROL, fState);
+                break;
+            case $"X":
+                KeyboardInput.SendKeyStroke((int)VirtualKeyCode.VK_F, fState);
+                break;
+            case $"Y":
+                KeyboardInput.SendKeyStroke((int)VirtualKeyCode.VK_V, fState);
+                break;
+            case $"LeftShoulder":
+                KeyboardInput.SendKeyStroke((int)VirtualKeyCode.VK_Q, fState);
+                break;
+            case $"RightShoulder":
+                KeyboardInput.SendKeyStroke((int)VirtualKeyCode.VK_E, fState);
+                break;
+            case $"DPadUp":
+                KeyboardInput.SendKeyStroke((int)VirtualKeyCode.UP, fState);
+                break;
+            case $"DPadDown":
+                KeyboardInput.SendKeyStroke((int)VirtualKeyCode.DOWN, fState);
+                break;
+            */case $"DPadLeft":
+                KeyboardInput.SendKeyStroke((int)VirtualKeyCode.VK_F, data.State);
+                break;/*
+            case $"DPadRight":
+                KeyboardInput.SendKeyStroke((int)VirtualKeyCode.RIGHT, fState);
+                break;
+            case $"Start":
+                KeyboardInput.SendKeyStroke((int)VirtualKeyCode.RETURN, fState);
+                break;
+            case $"Back":
+                KeyboardInput.SendKeyStroke((int)VirtualKeyCode.ESCAPE, fState);
+                break;*/
+            default:
+                break;
+        }
+    }
+}
--- MultiInput/Inputter/KeyboardInput.cs
+++ MultiInput/Inputter/KeyboardInput.cs
@@ -1,11 +1,5 @@
-﻿using System.Net.Sockets;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Text.Unicode;
-using MultiInput.Enums;
-using MultiInput.Enums.Constants;
+﻿
 using MultiInput.Logging;
-using MultiInput.TCP;
 using WindowsInput;
 using WindowsInput.Native;
 
@@ -15,114 +9,59 @@ namespace MultiInput.Inputter;
 public class KeyboardInput
 {
     private static InputSimulator _inputSimulator = new InputSimulator();
-    private static List<int> PressedKeys = new List<int>();
+    private static List<int> _pressedKeys = new List<int>();
 
     public void Handle(string data,int count){
         DebugLog.WriteDebugMessage($"Received: {data}");
         if (count > 3) {
-            if (data.Equals("ping", StringComparison.InvariantCultureIgnoreCase)) {
-                Console.WriteLine($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| Alive Ping Received");
-                DebugLog.WriteDebugMessage($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| Alive Ping Received");
+            
+            Data? validData = HandlerUtils.ValidateData(data);
+            if (validData is null) {
                 return;
             }
             
-            var strs = data.Split('.');
-
-
-            if (!int.TryParse(strs[0], out int inputType)) {
-                Console.WriteLine($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| Failed To Parse Input Type. DATA:{data}");
-                DebugLog.WriteDebugMessage($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| Failed To Parse Input Type. DATA:{data}");
-                return;
-            }
-            string fKey = strs[1];
-            if (fKey == string.Empty) {
-                Console.WriteLine($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| Failed To Parse Key. DATA:{data}");
-                DebugLog.WriteDebugMessage($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| Failed To Parse Key. DATA:{data}");
-                return;
-            }
-            if (!int.TryParse(strs[2], out int fState)) {
-                Console.WriteLine($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| Failed To Parse State. DATA:{data}");
-                DebugLog.WriteDebugMessage($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| Failed To Parse State. DATA:{data}");
-                return;
-            }
-            
-            if (inputType is (int)InputType.Keyboard) {
-                if (!int.TryParse(fKey, out int fKeyInt)) {
-                    Console.WriteLine($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| Failed To Parse Key. DATA:{data}");
-                    DebugLog.WriteDebugMessage($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| Failed To Parse Key. DATA:{data}");
+            switch (validData.InputType) {
+                case InputType.Keyboard:
+                {
+                    if (!int.TryParse(validData.Key, out int fKeyInt)) {
+                        Console.WriteLine($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| Failed To Parse Key. DATA:{data}");
+                        DebugLog.WriteDebugMessage($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| Failed To Parse Key. DATA:{data}");
+                        return;
+                    }
+                
+                    Console.WriteLine($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| KEY:{validData.Key} STATE:{validData.State}");
+                    DebugLog.WriteDebugMessage($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| KEY:{validData.Key} STATE:{validData.State}");
+                    SendKeyStroke(fKeyInt, validData.State);
                     return;
                 }
-                
-                Console.WriteLine($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| KEY:{fKey} STATE:{fState}");
-                DebugLog.WriteDebugMessage($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| KEY:{fKey} STATE:{fState}");
-                SendKeyStroke(fKeyInt, fState);
-                return;
-            }
-            
-            if (inputType is (int)InputType.Controller) {
-                Console.WriteLine($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| CONTROLLER INPUT | KEY:{fKey} STATE:{fState}");
-                DebugLog.WriteDebugMessage($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| CONTROLLER INPUT | KEY:{fKey} STATE:{fState}");
-                switch (fKey) {
-                    case $"A":
-                        SendKeyStroke((int)VirtualKeyCode.SPACE, fState);
-                        break;
-                    case $"B":
-                        SendKeyStroke((int)VirtualKeyCode.LCONTROL, fState);
-                        break;
-                    case $"X":
-                        SendKeyStroke((int)VirtualKeyCode.VK_F, fState);
-                        break;
-                    case $"Y":
-                        SendKeyStroke((int)VirtualKeyCode.VK_V, fState);
-                        break;
-                    case $"LeftShoulder":
-                        SendKeyStroke((int)VirtualKeyCode.VK_Q, fState);
-                        break;
-                    case $"RightShoulder":
-                        SendKeyStroke((int)VirtualKeyCode.VK_E, fState);
-                        break;
-                    case $"DPadUp":
-                        SendKeyStroke((int)VirtualKeyCode.UP, fState);
-                        break;
-                    case $"DPadDown":
-                        SendKeyStroke((int)VirtualKeyCode.DOWN, fState);
-                        break;
-                    case $"DPadLeft":
-                        SendKeyStroke((int)VirtualKeyCode.LEFT, fState);
-                        break;
-                    case $"DPadRight":
-                        SendKeyStroke((int)VirtualKeyCode.RIGHT, fState);
-                        break;
-                    case $"Start":
-                        SendKeyStroke((int)VirtualKeyCode.RETURN, fState);
-                        break;
-                    case $"Back":
-                        SendKeyStroke((int)VirtualKeyCode.ESCAPE, fState);
-                        break;
-                }
+                case InputType.Controller:
+                    Console.WriteLine($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| CONTROLLER INPUT | KEY:{validData.Key} STATE:{validData.State}");
+                    DebugLog.WriteDebugMessage($"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}| CONTROLLER INPUT | KEY:{validData.Key} STATE:{validData.State}");
+                    HandlerUtils.HandleController(validData);
+                    break;
             }
         }
     }
 
     public static List<int> GetUpVirtualKeys(){
-        return PressedKeys;
+        return _pressedKeys;
     }
     
     
     
     public static void SendKeyStroke(int key,int state){
         if (state == 1) {
-            PressedKeys.Add(key);
+            _pressedKeys.Add(key);
             _inputSimulator.Keyboard.KeyDown((VirtualKeyCode)key);
             return;
         }
 
-        PressedKeys.Remove(key);
+        _pressedKeys.Remove(key);
         _inputSimulator.Keyboard.KeyUp((VirtualKeyCode)key);
     }
 
     public static void ReleaseAll(){
-        foreach (var virtualKey in PressedKeys) {
+        foreach (var virtualKey in _pressedKeys) {
             SendKeyStroke(virtualKey,0);
         }
     }
